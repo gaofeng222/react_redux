3fb976be1d21ebf56883c1c57c50763772ef458a
gaofeng222 <596642721@qq.com>
2018-10-14 23:30:39 +0800

    all ok

--- src/components/List.js
+++ src/components/List.js
@@ -4,16 +4,40 @@
 import React from 'react';
 
 const List = (props) => {
+    const list = props.listItems.map((item, i)=>(
+        <li style={styleDes.liStyle} key={i}>
+            <div
+                style={
+                    item.done
+                        ? styleDes.divNoneStyle
+                        : styleDes.divStyle
+                }
+                onClick={() => props.onListItemClick(i)}
+            >{item.val}</div>
+            <span onClick={() => props.onListDeleteClick(i)} className="btn btn-danger">x</span>
+        </li>
+    ));
     return (
         <div>
-            <ul>
-                {
-                    props.listItems.map( (el,i) => (
-                        <li key={i}><span>{el}</span></li>
-                    ))
-                }
-            </ul>
+            <ul>{list}</ul>
         </div>
     )
+}
+const styleDes = {
+    liStyle: {
+        margin: '20px auto'
+    },
+    divNoneStyle: {
+        textDecoration: 'none',
+        fontSize: '20px',
+        width: 350,
+        display: 'inline-block'
+    },
+    divStyle: {
+        textDecoration: 'line-through',
+        fontSize: '20px',
+        width: 350,
+        display: 'inline-block'
+    }
 }
 export default List
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -8,20 +8,34 @@ import Input from './Input'
 class ToDoList extends React.Component {
     componentWillMount(){
         this.setState({ // add an array of strings to state.
-            list: ['thing1', 'thing2', 'thing3'],
+            list: [{val:'More snacks', done: false}, {val:'More money', done: false}, {val:'More traveling', done: false}],
             newToDo : 'test',
         })
     }
     onInputChange (event) {
         this.setState({ newToDo: event.target.value });
     }
-    onInputSubmit(event){
-        event.preventDefault()
-        console.log(11111)
+    onInputSubmit (event) {
+        event.preventDefault();
         this.setState((previousState)=>({
-            list: [...previousState.list, previousState.newToDo ],
-            newToDo : ''
+            list: [...previousState.list,  {val: previousState.newToDo, done: true }], // notice the change here
+            newToDo: ''
         }));
+    }
+    onListItemClick (i){ // takes the index of the element to be updated
+        this.setState((previousState)=>({
+            list: [
+                ...previousState.list.slice(0, i), // slice returns a new array without modifying the existing array. Takes everything up to, but not including, the index passed in.
+                Object.assign({}, previousState.list[i], {done: !previousState.list[i].done}), // Object.assign is a new ES6 feature that creates a new object based on the first param (in this case an empty object). Other objects can be passed in and will be added to the first object without being modified.
+                ...previousState.list.slice(i+1) // takes everything after the index passed in and adds it to the array.
+            ]
+        }))
+    }
+    onListDeleteClick(i){
+        console.log(11111)
+        this.setState((previousState) => ({
+            list : [...previousState.list.slice(0, i),...previousState.list.slice(i+1)]
+        }))
     }
     render(){
         return (
@@ -32,7 +46,7 @@ class ToDoList extends React.Component {
                             <h1>My To Do List</h1>
                             <hr/>
                             <Input value={this.state.newToDo} onInputChange={this.onInputChange.bind(this)} onInputSubmit = {this.onInputSubmit.bind(this)}/>
-                            <List listItems={this.state.list}/>
+                            <List listItems={this.state.list} onListItemClick={this.onListItemClick.bind(this)} onListDeleteClick={this.onListDeleteClick.bind(this)}/>
                         </div>
                     </div>
                 </div>
