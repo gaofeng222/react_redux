9facaf74a9ec7edfe6a4d5a6e970219633b355db
gaofeng222 <596642721@qq.com>
2018-10-14 20:36:08 +0800

    2 commit

---
+++ src/components/ToDoList.js
@@ -0,0 +1,15 @@
+/**
+ * Created by gaofeng on 2018/10/14.
+ */
+import React from 'react';
+
+class ToDoList extends React.Component {
+    render(){
+        return (
+            <div>
+                To do list
+            </div>
+        )
+    }
+}
+export default ToDoList
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (env,argv) => {
+    const devMode = argv.mode !== 'production';
+    return {
+        entry : [
+            "babel-polyfill",
+            path.join(__dirname,'./src/index.js')
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {}
+                        }
+                    ]
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'postcss-loader'
+                    ]
+                },
+                {
+                    test: /\.less$/,
+                    use: [
+                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'postcss-loader',
+                        'less-loader',
+                    ]
+                }
+            ]
+        },
+        plugins : [
+            new CleanWebpackPlugin(['dist']),
+            new HtmlWebPackPlugin({
+                template: "./src/index.html",
+                filename: "./index.html"
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+                chunkFilename: "[id].css"
+            })
+        ]
+    }
+}
